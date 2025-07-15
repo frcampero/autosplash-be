8c3d21a694b84e98faa411ae397f17d6dc2a6c2d
Federico Campero <89789363+frcampero@users.noreply.github.com>
2025-07-14 23:14:41 -0300

    Fix routes

--- routes/customerRoutes.js
+++ routes/customerRoutes.js
@@ -14,13 +14,17 @@ const verifyToken = require('../middleware/authMiddleware');
 const validateCustomer = require('../middleware/customerValidator');
 const handleValidation = require('../middleware/handleValidation');
 
+// Routes
 router.post('/', verifyToken, validateCustomer, handleValidation, createCustomer);
 router.get('/', verifyToken, getCustomers);
 router.get('/top', verifyToken, getTopCustomers);
+
+//Specific route placed before dynamic :id
+router.get('/:id/stats', verifyToken, getCustomerStats);
+
+// Dynamic routes
 router.get('/:id', verifyToken, getCustomerById);
 router.put('/:id', verifyToken, updateCustomer);
 router.delete('/:id', verifyToken, deleteCustomer);
-router.get('/:id/stats', verifyToken, getCustomerStats);
-
 
 module.exports = router;
