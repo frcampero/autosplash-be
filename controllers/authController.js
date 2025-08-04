7424afacc3053ea47b265438a53d3ea9fedbbd0f
Federico Campero <89789363+frcampero@users.noreply.github.com>
2025-08-04 19:16:34 -0300

    fix: enable cookies cross-origin

--- controllers/authController.js
+++ controllers/authController.js
@@ -48,7 +48,7 @@ const login = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "Strict",
+      sameSite: "none",
       maxAge: 1000 * 60 * 60 * 12,
     });
 
